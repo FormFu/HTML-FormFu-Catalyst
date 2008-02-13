04e2f5fc13b2a201873bde25ca7929162c4a5da6
fireartist <fireartist@gmail.com>
2008-02-13 19:10:38 +0000

    changed StashUserID to StashContextMethods for a more generic solution
    git-svn-id: file:///home/cafranks/github/googlecode/trunk/HTML-FormFu-Catalyst@881 3452f2c1-2087-41cb-87be-0eefa6d42935

---
+++ lib/HTML/FormFu/Plugin/Catalyst/StashContextMethods.pm
@@ -0,0 +1,64 @@
+package HTML::FormFu::Plugin::Catalyst::StashContextMethods;
+
+use strict;
+use base 'HTML::FormFu::Plugin';
+use Class::C3;
+use Carp qw( croak );
+
+use HTML::FormFu::Attribute qw/ mk_accessors /;
+
+__PACKAGE__->mk_accessors(qw/ stash_key methods /);
+
+sub process {
+    my ( $self ) = @_;
+
+    for my $arg (qw/ stash_key methods /) {
+        croak "'$arg' config missing"
+            if !defined $self->$arg;
+    }
+
+    my $form    = $self->form;
+    my $value   = $form->{context};
+    my $methods = $self->methods;
+
+    $methods = [$methods] if ref $methods ne 'ARRAY';
+
+    for my $method ($methods) {
+        $value = $value->$method;
+    }
+
+    $form->stash->{ $self->stash_key } = $value;
+
+    return;
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+HTML::FormFu::Plugin::Catalyst::StashContextMethods - stash context values
+
+=head1 SYNOPSIS
+
+    ---
+    plugins:
+      - type: 'Catalyst::StashContextMethods'
+        stash_key: user_id
+        methods: [user, obj, id]
+
+=head1 SEE ALSO
+
+Is a sub-class of, and inherits methods from L<HTML::FormFu::Plugin>
+
+L<HTML::FormFu::FormFu>
+
+=head1 AUTHOR
+
+Carl Franks C<cfranks@cpan.org>
+
+=head1 LICENSE
+
+This library is free software, you can redistribute it and/or modify it under
+the same terms as Perl itself.
