f0bae8c418f2832819672b0cb473f9e6cc76ef85
fireartist <fireartist@gmail.com>
2008-02-13 19:20:29 +0000

    added Makefile.PL and HTML/FormFu/Catalyst for version no. / pod
    git-svn-id: file:///home/cafranks/github/googlecode/trunk/HTML-FormFu-Catalyst@882 3452f2c1-2087-41cb-87be-0eefa6d42935

---
+++ lib/HTML/FormFu/Catalyst.pm
@@ -0,0 +1,33 @@
+package HTML::FormFu::Catalyst;
+use strict;
+
+our $VERSION = '0.02004';
+
+1;
+
+=head1 NAME
+
+HTML::FormFu::Catalyst
+
+=head1 DESCRIPTION
+
+This module isn't used directly, it just bundles the following modules:
+
+=over
+
+=item L<HTML::FormFu::Plugin::Catalyst::StashContextMethods>
+
+=back
+
+=head1 SEE ALSO
+
+L<HTML::FormFu::FormFu>, L<Catalyst::Controller::HTML::FormFu>
+
+=head1 AUTHOR
+
+Carl Franks C<cfranks@cpan.org>
+
+=head1 LICENSE
+
+This library is free software, you can redistribute it and/or modify it under
+the same terms as Perl itself.
